0cba929f14447a9e30687adc39bc3e53c12bf560
liqxm <liqxm@yonyou.com>
2019-10-16 17:11:46 +0800

    样式1px问题

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <Header :seller="seller"></Header>
-    <div>
+    <div class="tab border-1px">
         <router-link to="/goods">商品</router-link>
         <router-link to="/ratings">评价</router-link>
         <router-link to="/seller">商家</router-link>
@@ -27,13 +27,23 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus">
+  @import "./common/stylus/mixin.styl"
+
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    // border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+    border-1px(rgba(7, 17, 27, 0.1))
+    .tab-item
+      flex: 1
+      text-align: center
+      & > a
+        display: block
+        font-size: 14px
+        color: rgb(77, 85, 93)
+        &.active
+          color: rgb(240, 20, 20)
 </style>
