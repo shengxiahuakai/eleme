671451dc6788f348f0e4df94a0e81541cbccfa60
liqxm <liqxm@yonyou.com>
2019-10-18 16:41:55 +0800

    mock 配置

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+const mockdata = require('./mock/test.json');
 module.exports = {
     outputDir: 'dist', // 打包的目录
     lintOnSave: true, // 在保存时校验格式
@@ -6,6 +7,11 @@ module.exports = {
       open: true, // 启动服务后是否打开浏览器
       host: '0.0.0.0',
       port: 8080, // 服务端口
+      before(app){
+        app.get('/goods/list',(req,res,next)=>{
+          res.json(mockdata);
+        })
+      },
       https: false,
       hotOnly: false,
       proxy: null, // 设置代理
